e7e0c5f6c25aa642118edeb0a43a7d8ad44fd909
Beth Ruff <beth.ruff2@gmail.com>
2020-06-01 20:36:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ActiveRecord::Base
   validates :title, presence: true 
   validates :content, length: { minimum: 10}
   validates :summary, length: { maximum: 50 }
-  validates :category, inclusion: { in: %w( )
 end
