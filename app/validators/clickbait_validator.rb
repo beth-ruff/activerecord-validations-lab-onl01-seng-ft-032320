a98b94dbb3e47b0df7c98f33d593bfde9aa9999d
Beth Ruff <beth.ruff2@gmail.com>
2020-06-01 20:44:25 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -2,6 +2,8 @@ class ClickbaitValidator < ActiveModel::Validator
   
   def validate(record)
     if record.title 
+      reqs = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+      
   end 
   
 end 
